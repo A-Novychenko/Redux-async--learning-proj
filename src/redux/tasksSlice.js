45e16187094abe735a5738ebba301cc31b9090dc
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-02 19:40:14 +0300

    change initialState

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,21 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
-const tasksInitialState = [
-  { id: 0, text: 'Learn HTML and CSS', completed: true },
-  { id: 1, text: 'Get good at JavaScript', completed: true },
-  { id: 2, text: 'Master React', completed: false },
-  { id: 3, text: 'Discover Redux', completed: false },
-  { id: 4, text: 'Build amazing apps', completed: false },
-];
-
 const tasksSlice = createSlice({
   name: 'tasks',
-  initialState: tasksInitialState,
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     addTask: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.items.push(action.payload);
       },
       prepare(text) {
         return {
@@ -29,20 +25,21 @@ const tasksSlice = createSlice({
     },
 
     deleteTask(state, action) {
-      const ind = state.findIndex(task => task.id === action.payload);
-      state.splice(ind, 1);
+      const ind = state.items.findIndex(task => task.id === action.payload);
+      state.items.splice(ind, 1);
     },
-    toggleCompleted(state, action) {
-      for (const task of state) {
+    toggleCompleted({ items }, action) {
+      for (const task of items) {
         if (task.id === action.payload) {
           task.completed = !task.completed;
           break;
         }
       }
     },
-    deleteAll() {
-      return [];
+    deleteAll(state) {
+      state.items = [];
     },
+    fetchTask() {},
   },
 });
 
