256abdcb8d2f3bcd437b1da88dd64b032cb8fc8b
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-03 22:54:28 +0300

    completetd

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,11 +3,17 @@ import { useDispatch } from 'react-redux';
 import { deleteTask, toggleCompleted } from 'redux/operations';
 
 import css from './Task.module.css';
+import { useState } from 'react';
 
 export const Task = ({ task }) => {
+  const [isBtnActive, setIsBtnActive] = useState(false);
+
   const dispatch = useDispatch();
 
-  const handleDelete = () => dispatch(deleteTask(task.id));
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id));
+    setIsBtnActive(true);
+  };
   const handleToggle = () => dispatch(toggleCompleted(task));
 
   return (
@@ -19,7 +25,7 @@ export const Task = ({ task }) => {
         onChange={handleToggle}
       />
       <p className={css.text}>{task.task}</p>
-      <button className={css.btn} onClick={handleDelete}>
+      <button className={css.btn} onClick={handleDelete} disabled={isBtnActive}>
         <MdClose size={24} />
       </button>
     </div>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,4 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { statusFilters } from './constants';
 
 export const selectTasks = state => state.tasks.items;
@@ -5,24 +6,22 @@ export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 export const selectStatusFilter = state => state.filters.status;
 
-export const selectVisibleTasks = state => {
-  const tasks = selectTasks(state);
-  const statusFilter = selectStatusFilter(state);
-
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
   }
-};
+);
 
-export const selectCount = state => {
-  const tasks = selectTasks(state);
-
-  return tasks.reduce(
+export const selectCount = createSelector([selectTasks], tasks =>
+  tasks.reduce(
     (acc, task) => {
       if (task.completed) {
         acc.completed += 1;
@@ -32,5 +31,5 @@ export const selectCount = state => {
       return acc;
     },
     { active: 0, completed: 0 }
-  );
-};
+  )
+);
